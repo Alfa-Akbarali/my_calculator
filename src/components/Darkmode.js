089de2bfa56b5145e915969dd261621f8abc0719
Alfa_001 <104311979+Alfa-Akbarali@users.noreply.github.com>
2024-05-23 17:56:00 +0500

    Add files via upload

---
+++ src/components/Darkmode.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Dark from '../assets/images/moon.svg'
+import Light from '../assets/images/sun.svg'
+import './css/darkmode.css'
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
+
+export default function Darkmode() {
+    // darkmode switch
+    const [darkmode, setDarkmode] = useState(cookies.get('darkmode') === 'false' ? false : true);
+    cookies.set('darkmode', darkmode, { path: '/' });
+
+    function LightOn() {
+        setDarkmode(false);
+    }
+    function LightOff() {
+        setDarkmode(true)
+    }
+    try {
+        var bodyTag = document.getElementById("body")
+        darkmode === true ? bodyTag.classList.remove("darkmodeDeAct") : bodyTag.classList.add("darkmodeDeAct")
+    } catch (err) { }
+    return (
+        <div className="darkmode">
+
+
+            <img className={darkmode ? "ON" : "OFF"} onClick={LightOff} src={Dark} alt="Dark" />
+            <img className={darkmode ? "OFF" : "ON"} onClick={LightOn} src={Light} alt="Light" />
+
+        </div >
+    )
+}
